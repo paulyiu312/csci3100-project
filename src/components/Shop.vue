001bb1f2ebe027892b2dad7de247069c3c3bdb7a
minglaitin <lilyming2000@gmail.com>
2021-03-19 16:32:29 +0800

    add some template html in Shop.vue

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,12 +1,41 @@
 <template>
   <div id="shop">
-
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <p>{{itemId}}</p>
+        <p>{{itemType}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-name: "Shop"
+  name: "Shop",
+  data() {
+    return {
+      items: [
+        {
+          itemId: 1001,
+          itemType: 'avatar',
+          itemName: 'Cat',
+          itemImage: '../src/assets/logo.png'
+        },
+        {
+          itemId: 1002,
+          itemType: 'avatar',
+          itemName: 'Dog',
+          itemImage: '../src/assets/logo.png'
+        },
+        {
+          itemId: 1003,
+          itemType: 'avatar',
+          itemName: 'Mouse',
+          itemImage: '../src/assets/logo.png'
+        }
+      ]
+    }
+  }
 }
 </script>
 
