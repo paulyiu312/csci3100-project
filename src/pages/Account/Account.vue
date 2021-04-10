6769b7f1a9606eafe07d1364eb2b5e2ea81eddf1
193934248 <62467832+193934248@users.noreply.github.com>
2021-04-10 09:24:35 +0800

    Update Account.vue

--- src/pages/Account/Account.vue
+++ src/pages/Account/Account.vue
@@ -3,8 +3,13 @@
     <NavBar></NavBar>
     <div class="main-content">
       <h1>Account</h1>
-    </div>
-  </div>
+      <div>
+          <img :src="user.avatar">
+          <p2> UserID: {{ user.userID }} </p2>
+          <p2> Last Active Time: {{ user.lastActiveTime }} </p2>
+          <p2> Highest Score: {{ user.accumulatedScore }} </p2>
+          <p2> Coins: {{ user.coins }} </p2>
+      </div>
 </template>
 
 <script>
